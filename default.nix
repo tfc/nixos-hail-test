a99edd9b564be601e9efd399191801ef36e9ca0e
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2019-08-15 11:08:54 +0200

    Make it a jobset attribute

--- default.nix
+++ default.nix
@@ -4,14 +4,13 @@
   pkgs ? import nixpkgs {}
 }:
 
-let
-  configBuilder = import nixos;
-  config = configBuilder { configuration = ./configuration.nix; };
-in
-  pkgs.writeScriptBin "activate" ''
-    exec -a systemd-run ${pkgs.systemd}/bin/systemd-run \
-      --description "Hail: Activate new configuration" \
-      -E PATH=$PATH \
-      -E LOCALE_ARCHIVE=$LOCALE_ARCHIVE \
-      ${config.system}/bin/switch-to-configuration switch
-  ''
+rec {
+  config = import nixos { configuration = ./configuration.nix; };
+  activator = pkgs.writeScriptBin "activate" ''
+      exec -a systemd-run ${pkgs.systemd}/bin/systemd-run \
+        --description "Hail: Activate new configuration" \
+        -E PATH=$PATH \
+        -E LOCALE_ARCHIVE=$LOCALE_ARCHIVE \
+        ${config.system}/bin/switch-to-configuration switch
+    '';
+}
