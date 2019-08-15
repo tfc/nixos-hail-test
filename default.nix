e8e0ae2c20eb63d5e649d476b991ff8125cc72aa
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2019-08-15 12:22:16 +0200

    use nixos function from pkgs

--- default.nix
+++ default.nix
@@ -1,16 +1,15 @@
 {
   nixpkgs ? <nixpkgs>,
-  nixos ? <nixpkgs/nixos>,
   pkgs ? import nixpkgs {}
 }:
 
 rec {
-  config = import nixos { configuration = ./configuration.nix; };
+  config = pkgs.nixos ./configuration.nix;
   activator = pkgs.writeScriptBin "activate" ''
       exec -a systemd-run ${pkgs.systemd}/bin/systemd-run \
         --description "Hail: Activate new configuration" \
         -E PATH=$PATH \
         -E LOCALE_ARCHIVE=$LOCALE_ARCHIVE \
-        ${config.system}/bin/switch-to-configuration switch
+        ${config.toplevel}/bin/switch-to-configuration switch
     '';
 }
